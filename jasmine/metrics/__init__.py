b0a617ec3f5029055ff1b6cd230b5ba0988f12de
Jaseem Paloth <jaseempalothjp@gmail.com>
2025-07-27 03:03:44 +0530

    Add regression metrics to module exports

--- jasmine/metrics/__init__.py
+++ jasmine/metrics/__init__.py
@@ -1,5 +1,6 @@
 
 from .loss import Loss, MSELoss, MAELoss, RMSELoss, CrossEntropyLoss
+from ._regression import mean_absolute_error, mean_squared_error, root_mean_squared_error
 
 __all__ = [
     "Loss",
