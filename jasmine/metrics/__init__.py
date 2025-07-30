587feb6fa6d6faeccbdce7562e1dcf13e4b30352
Jaseem Paloth <jaseempalothjp@gmail.com>
2025-07-31 02:15:41 +0530

    Add classification metrics to the public API

--- jasmine/metrics/__init__.py
+++ jasmine/metrics/__init__.py
@@ -1,9 +1,12 @@
 from ._regression import mean_absolute_error, mean_squared_error, root_mean_squared_error, r2_score
-
+from ._classification import binary_cross_entropy, categorical_cross_entropy, accuracy_score
 __all__ = [
     "mean_absolute_error",
     "mean_squared_error",
     "root_mean_squared_error",
-    "r2_score"
+    "r2_score",
+    "binary_cross_entropy",
+    "categorical_cross_entropy",
+    "accuracy_score"
 ]
 
