d0e336d0976593948f3452e2cb2d29b08ade9bc5
Jaseem Paloth <jaseempalothjp@gmail.com>
2025-08-06 03:19:27 +0530

    Refactor Sphinx configuration to clear static path settings for future customization

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_static_path = []  # Empty for now, will add files later if needed
 
 # Theme options
 html_theme_options = {
@@ -60,7 +60,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []  # Empty for now, will add files later if needed
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
