b92c7ef2776d07afa3f153ad081e1c152bcb79a3
Jaseem Paloth <jaseempalothjp@gmail.com>
2025-08-07 21:37:00 +0530

    Add distance metrics to classification module and update exports

--- jasmine/metrics/__init__.py
+++ jasmine/metrics/__init__.py
@@ -1,5 +1,8 @@
 from ._regression import mean_absolute_error, mean_squared_error, root_mean_squared_error, r2_score
-from ._classification import binary_cross_entropy, categorical_cross_entropy, accuracy_score
+
+from ._classification import binary_cross_entropy, categorical_cross_entropy, accuracy_score, \
+    euclidean_distance, manhattan_distance
+
 __all__ = [
     "mean_absolute_error",
     "mean_squared_error",
@@ -7,6 +10,8 @@ __all__ = [
     "r2_score",
     "binary_cross_entropy",
     "categorical_cross_entropy",
-    "accuracy_score"
+    "accuracy_score",
+    "euclidean_distance",
+    "manhattan_distance"
 ]
 
