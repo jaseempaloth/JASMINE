c34be9a4cbfe5fb234371345f8321e2e8d7cbefe
Jaseem Paloth <jaseempalothjp@gmail.com>
2025-08-12 18:23:14 +0530

    Add SVMClassifier to module exports in __init__.py

--- jasmine/classification/__init__.py
+++ jasmine/classification/__init__.py
@@ -1,7 +1,9 @@
 from .logistic_regression import LogisticRegression
 from .knn_classifier import KNNClassifier
+from .svm_classifier import SVMClassifier
 
 __all__ = [
     'LogisticRegression',
-    'KNNClassifier'
+    'KNNClassifier',
+    'SVMClassifier'
 ]
